05b51c5f5344036835b529c7668347033aae3803
Rob <robe16@hotmail.co.uk>
2017-02-06 09:22:19 +0000

    Removal of redundant files.

--- src/web/html/static/js/generic_code.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".dropdown-toggle").dropdown();
-});
-
-function hidePopup()
-    {
-        document.getElementById('message_popup').className = "message viewport_centre hidden";
-        if (document.getElementById('msg_title').innerHTML == 'Success') {window.location.href = '/web/home';}
-    }
